73936191f51edf088fe9c8eaf0b7a3ebf49f9044
Nastia <adruts@mail.ru>
2023-12-16 22:02:54 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 3f; // Скорость перемещения врага
+    private float heightChangeSpeed = 2f; // Скорость изменения высоты врага
+    private float shootingInterval = 2f; // Интервал между выстрелами врага
+    private float shootingTimer = 0f; // Таймер для отслеживания интервала выстрелов
+    public GameObject bulletPrefab; // Префаб пули врага
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Вычисляем новую позицию врага
+        Vector3 newPosition = transform.position + new Vector3(speed * Time.deltaTime, Mathf.Sin(Time.time * heightChangeSpeed), 0);
+
+        // Применяем новую позицию врага
+        transform.position = newPosition;
+
+        // Проверяем, прошло ли достаточно времени для выстрела
+        shootingTimer += Time.deltaTime;
+        if (shootingTimer >= shootingInterval)
+        {
+            // Сбрасываем таймер выстрелов
+            shootingTimer = 0f;
+
+            // Создаем пулю врага
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            // Задаем направление движения пули
+            bullet.GetComponent<Rigidbody>().velocity = Vector3.left * 10f;
+        }
     }
 }
