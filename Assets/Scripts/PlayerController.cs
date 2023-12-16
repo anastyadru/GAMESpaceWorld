83f61fe3ab67c13cdc894416f334f165d99c0a16
Nastia <adruts@mail.ru>
2023-12-16 16:11:15 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    private float speed = 40f; // Скорость перемещения игрока
 
     void Update()
     {
-        
+        // Получаем текущие координаты мыши по горизонтали
+        float mouseX = Input.GetAxis("Mouse X");
+
+        // Вычисляем новую позицию игрока
+        Vector3 newPosition = transform.position + new Vector3(mouseX * speed * Time.deltaTime, 0, 0);
+
+        // Ограничиваем движение игрока по горизонтали
+        newPosition.x = Mathf.Clamp(newPosition.x, -100f, 100f);
+
+        // Применяем новую позицию игрока
+        transform.position = newPosition;
     }
 }
