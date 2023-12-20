a4e671aeb338584116450379bbe858f779f69b90
Nastia <adruts@mail.ru>
2023-12-20 11:06:11 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/EnemyController.cs
+++ Assets/ScriptableObject/EnemyController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public GameObject bulletPrefab;
+    public float moveSpeed = 2f; // Скорость перемещения
+    public float moveDistanceX = 30f; // Максимальное расстояние по оси X
+    public float moveDistanceY = 5f; // Максимальное расстояние по оси Y
     
+    public float shootInterval = 2f; // Интервал между выстрелами
+    private bool isMovingRight = true; // Флаг направления движения (вправо или влево)
+    private float moveTimer = 0f; // Таймер для перемещения
+    private float shootTimer = 0f; // Таймер для стрельбы
     
 }
