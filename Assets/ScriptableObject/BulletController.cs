dd63f5df037a191661e4dd4463bddb0af991722b
Nastia <adruts@mail.ru>
2023-12-20 17:55:23 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/BulletController.cs
+++ Assets/ScriptableObject/BulletController.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletController : MonoBehaviour
-{
-    public float speed = 100f; // Скорость пули
+{ 
+    public float speed; // Скорость пули
+    
+    // public float speed = 100f; // Скорость пули
 
-    void Update()
+    void Start()
     {
-        // Двигаем пулю вперед с постоянной скоростью
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
     }
+
+    // void Update()
+    // {
+        // Двигаем пулю вперед с постоянной скоростью
+        // transform.Translate(Vector3.up * speed * Time.deltaTime);
+    // }
 }
