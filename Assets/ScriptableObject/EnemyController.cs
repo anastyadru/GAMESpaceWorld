cba605c910d17ea58913e4ab96b4282db6afa0ff
Nastia <adruts@mail.ru>
2023-12-22 13:54:22 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/EnemyController.cs
+++ Assets/ScriptableObject/EnemyController.cs
@@ -4,61 +4,25 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float maxX = 10f;
-    public float minX = -10f;
-    public float maxY = 4f;
-    public float minY = -4f;
-    private int moveDirectionX = 1;
-    private int moveDirectionY = 1;
+    public float moveSpeed = 170f; // Скорость перемещения врага
+    private Vector3 randomDirection; // Случайное направление движения
 
-    void Start()
+    private void Start()
     {
-        // Запускаем корутину для случайного движения
-        StartCoroutine(RandomMovement());
+        // Задаем случайное начальное направление движения
+        randomDirection = new Vector3(Random.Range(-150f, 150f), Random.Range(-150f, 150f), 0f).normalized;
     }
 
-    void Update()
+    private void Update()
     {
-        // Движение влево и вправо
-        transform.Translate(Vector2.right * moveSpeed * moveDirectionX * Time.deltaTime);
+        // Перемещаем врага в заданном направлении
+        transform.Translate(randomDirection * moveSpeed * Time.deltaTime);
 
-        // Проверка ограничений по сцене по горизонтали
-        if (transform.position.x >= maxX)
+        // Если враг достиг границы экрана, меняем направление на случайное
+        if (transform.position.x < -150f || transform.position.x > 150f ||
+            transform.position.y < -150f || transform.position.y > 150f)
         {
-            moveDirectionX = -1;
-        }
-        else if (transform.position.x <= minX)
-        {
-            moveDirectionX = 1;
-        }
-
-        // Движение вверх и вниз
-        transform.Translate(Vector2.up * moveSpeed * moveDirectionY * Time.deltaTime);
-
-        // Проверка ограничений по сцене по вертикали
-        if (transform.position.y >= maxY)
-        {
-            moveDirectionY = -1;
-        }
-        else if (transform.position.y <= minY)
-        {
-            moveDirectionY = 1;
-        }
-    }
-
-    IEnumerator RandomMovement()
-    {
-        while (true)
-        {
-            // Ждем случайное время перед следующим изменением направления
-            yield return new WaitForSeconds(Random.Range(1f, 5f));
-
-            // Случайно выбираем направление движения по горизонтали
-            moveDirectionX = Random.Range(-1, 2);
-
-            // Случайно выбираем направление движения по вертикали
-            moveDirectionY = Random.Range(-1, 2);
+            randomDirection = new Vector3(Random.Range(-150f, 150f), Random.Range(-150f, 150f), 0f).normalized;
         }
     }
 }
