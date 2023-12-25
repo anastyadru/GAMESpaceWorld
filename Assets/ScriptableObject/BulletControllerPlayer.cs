68d599765d5743d500892343e85a6714571b80e5
Nastia <adruts@mail.ru>
2023-12-25 08:46:05 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/BulletControllerPlayer.cs
+++ Assets/ScriptableObject/BulletControllerPlayer.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class BulletControllerPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed; // Скорость пули
+
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
     }
 }
