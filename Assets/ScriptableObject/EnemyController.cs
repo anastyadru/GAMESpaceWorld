5521cbcded7fe38b5482bec725835374ad1e7aaa
Nastia <adruts@mail.ru>
2024-01-05 01:54:54 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/EnemyController.cs
+++ Assets/ScriptableObject/EnemyController.cs
@@ -20,21 +20,17 @@ public class EnemyController : MonoBehaviour
             Destroy(gameObject); // Уничтожаем врага
             Destroy(other.gameObject); // Уничтожаем то, с чем стоклнулись
             
-            // Проверяем, остались ли еще противники в текущей волне
-            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0) // Проверяем, остались ли еще противники в текущей волне
             {
                 currentWave++; // Увеличиваем номер текущей волны
-
-                // Если все волны пройдены, завершаем игру
-                if (currentWave == waveSizes.Length)
+                
+                if (currentWave == waveSizes.Length) // Если все волны пройдены, завершаем игру
                 {
-                    Debug.Log("Game Over");
-                    // Здесь можно добавить код для завершения игры
+                    Debug.Log("Game Over"); // Здесь можно добавить код для завершения игры
                     return;
                 }
-
-                // Генерируем новую волну противников
-                GenerateWave(waveSizes[currentWave], transform.position);
+                
+                GenerateWave(waveSizes[currentWave], transform.position); // Генерируем новую волну противников
             }
         }
     }
@@ -43,11 +39,8 @@ public class EnemyController : MonoBehaviour
     {
         for (int i = 0; i < enemyCount; i++)
         {
-            // Создаем нового противника
-            GameObject enemy = Instantiate(enemyPrefab, startPosition, transform.rotation);
-            
-            // Устанавливаем случайную позицию для противника
-            float randomX = Random.Range(-100f, 100f);
+            GameObject enemy = Instantiate(enemyPrefab, startPosition, transform.rotation); // Создаем нового противника
+            float randomX = Random.Range(-100f, 100f); // Устанавливаем случайную позицию для противника
             enemy.transform.position += new Vector3(randomX, 0, 0);
         }
     }
