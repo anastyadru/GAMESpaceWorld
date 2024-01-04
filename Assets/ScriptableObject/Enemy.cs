4e9c5af8da3bdbaccd3a0ecbe82a95924d24f800
Nastia <adruts@mail.ru>
2024-01-04 20:19:08 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/Enemy.cs
+++ Assets/ScriptableObject/Enemy.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 250f; // Скорость перемещения
+    private float smoothness = 3f; // Плавность движения
 
-    // Update is called once per frame
+    private Vector3 targetPosition; // Целевая позиция
+    
+    public GameObject lazerShot1;
+    public Transform lazerGun1;
+    private float nextShotTime; // Время следующего выстрела
+    
     void Update()
     {
+        if (Vector3.Distance(transform.position, targetPosition) < 10f) // Если враг близок к целевой позиции
+        {
+            GenerateNewTargetPosition(); // Генерируем новую целевую позицию
+        }
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime); // Плавно перемещаем к целевой позиции
         
+        if (Time.time > nextShotTime)
+        {
+            Shoot();
+        }
     }
+
+    void GenerateNewTargetPosition()
+    {
+        float randomX = Random.Range(-100f, 100f); // Генерируем случайное значение по оси X
+        targetPosition = transform.position + new Vector3(randomX, 0, 0); // Изначально целевая позиция равна текущей позиции + случайное значение по оси X
+        
+        targetPosition.x = Mathf.Clamp(targetPosition.x, -500f, -100f); // Ограничиваем движение врага по горизонтали
+    }
+    
+    void Shoot()
+    {
+        Instantiate(lazerShot1, lazerGun1.position, Quaternion.identity);
+        nextShotTime = Time.time + 4f;
+    }
+    
+    
+
 }
--- Assets/ScriptableObject/EnemyController.cs
+++ Assets/ScriptableObject/EnemyController.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private float speed = 250f; // Скорость перемещения
-    private float smoothness = 3f; // Плавность движения
-
-    private Vector3 targetPosition; // Целевая позиция
-    
-    public GameObject lazerShot1;
-    public Transform lazerGun1;
-    private float nextShotTime; // Время следующего выстрела
-    
     public GameObject enemyPrefab; // Объявляем переменную для префаба противника
     private int currentWave = 0; // Текущая волна
     private int[] waveSizes = { 3, 5, 4, 6, 8, 12, 8 }; // Количество противников в каждой волне
@@ -20,38 +11,9 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         GenerateWave(waveSizes[currentWave], transform.position);
-        GenerateNewTargetPosition();
+        // GenerateNewTargetPosition();
     }
 
-    void Update()
-    {
-        if (Vector3.Distance(transform.position, targetPosition) < 10f) // Если враг близок к целевой позиции
-        {
-            GenerateNewTargetPosition(); // Генерируем новую целевую позицию
-        }
-
-        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothness * Time.deltaTime); // Плавно перемещаем к целевой позиции
-        
-        if (Time.time > nextShotTime)
-        {
-            Shoot();
-        }
-    }
-
-    void GenerateNewTargetPosition()
-    {
-        float randomX = Random.Range(-100f, 100f); // Генерируем случайное значение по оси X
-        targetPosition = transform.position + new Vector3(randomX, 0, 0); // Изначально целевая позиция равна текущей позиции + случайное значение по оси X
-        
-        targetPosition.x = Mathf.Clamp(targetPosition.x, -500f, -100f); // Ограничиваем движение врага по горизонтали
-    }
-    
-    void Shoot()
-    {
-        Instantiate(lazerShot1, lazerGun1.position, Quaternion.identity);
-        nextShotTime = Time.time + 1.5f;
-    }
-    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("lazerShot") && other.gameObject == gameObject) // Проверяем тег объекта
