1bf5ab9390de333c3b8d8aa54973dedc0f2eafc0
Nastia <adruts@mail.ru>
2024-01-07 23:18:33 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/HealthManager.cs
+++ Assets/ScriptableObject/HealthManager.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-
+    [SerializeField] private Text HealthText;
+    
+    public float health;
 }
