fdfed720974cd8ef34c822c2ee9b9af01e53394d
Nastia <adruts@mail.ru>
2024-01-16 17:01:36 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/ScoreManager.cs
+++ Assets/ScriptableObject/ScoreManager.cs
@@ -6,19 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class ScoreManager : MonoBehaviour
 {
-    [SerializeField] private Text HighScoreText;
     [SerializeField] private Text ScoreText;
+    [SerializeField] private Text HighScoreText;
     
-    public float score;
-    public float highscore;
+    public float score = 0f;
+    public float highscore = 0f;
 
 	void Start()
     {
-        
+        UpdateScoreText();
+        // UpdateHighScoreText;
     }
 
     void Update()
     {
         
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("lazerShot")) // Проверяем тег объекта
+        {
+            if (other.GetComponent<Collider>().gameObject.CompareTag("Enemy")) // Проверяем тег объекта, к которому прикреплен коллайдер
+            {
+                score += 2;
+                UpdateScoreText();
+            }
+        }
+    }
+    
+    private void UpdateScoreText()
+    {
+        ScoreText.text = "SCORE: " + score.ToString();
     }
 }
