393544bc20aad7ef3006464ac73cdac2d7151f2d
Nastia <adruts@mail.ru>
2024-01-19 21:09:54 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/BonusManager.cs
+++ Assets/ScriptableObject/BonusManager.cs
@@ -46,25 +46,7 @@ public class BonusManager : MonoBehaviour
     {
         if (Input.GetButton("Fire2"))
         {
-            // Находим все объекты на сцене с тегом "Enemy"
-            GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
-
-            // Проходимся по каждому объекту и наносим урон
-            foreach (GameObject enemy in enemies)
-            {
-                // Получаем компонент EnemyHealth из объекта противника
-                EnemyHealth enemyHealth = enemy.GetComponent<EnemyHealth>();
-
-                // Проверяем, что объект противника имеет компонент EnemyHealth
-                if (enemyHealth != null)
-                {
-                    // Вычисляем урон, умножая базовый урон на 2.5
-                    float damage = enemyHealth.baseDamage * 2.5f;
-
-                    // Наносим урон противнику
-                    enemyHealth.TakeDamage(damage);
-                }
-            }
+         
         }
     }
 }
