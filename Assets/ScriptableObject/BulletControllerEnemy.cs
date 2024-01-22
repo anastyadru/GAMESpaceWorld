7d2527065af5397cf145720c0fe3cfc79dc7c0f1
Nastia <adruts@mail.ru>
2024-01-23 00:00:39 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/BulletControllerEnemy.cs
+++ Assets/ScriptableObject/BulletControllerEnemy.cs
@@ -6,7 +6,7 @@ public class BulletControllerEnemy : MonoBehaviour
 {
     public float speed = 100; // Скорость пули
 
-    void Start()
+    void Update()
     {
         transform.Translate(Vector3.back * speed * Time.deltaTime);
     }
