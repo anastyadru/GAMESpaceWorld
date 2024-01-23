cd3d70efc418e01cdb668cd00f9d162224e07e19
Nastia <adruts@mail.ru>
2024-01-23 22:17:10 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/WindowManager.cs
+++ Assets/WindowManager.cs
@@ -4,6 +4,62 @@ using UnityEngine;
 
 public class WindowManager : MonoBehaviour
 {
-    // private static WindowManager instance;
-    // private Stack<Window> windowStack = new Stack<Window>();
+    private static WindowManager instance;
+    private Stack<Window> windowStack = new Stack<Window>();
+    
+    public static WindowManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<WindowManager>();
+                if (instance == null)
+                {
+                    GameObject container = new GameObject("WindowManager");
+                    instance = container.AddComponent<WindowManager>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        windowStack = new Stack<BaseWindow>();
+    }
+
+    public void Open(WindowID windowID)
+    {
+        BaseWindow window = GetWindow(windowID);
+        if (window != null)
+        {
+            window.gameObject.SetActive(true);
+            window.OnOpened();
+            windowStack.Push(window);
+        }
+    }
+
+    public void CloseLast()
+    {
+        if (windowStack.Count > 0)
+        {
+            BaseWindow window = windowStack.Pop();
+            window.OnClosed();
+            window.gameObject.SetActive(false);
+        }
+    }
+
+    private BaseWindow GetWindow(WindowID windowID)
+    {
+        switch (windowID)
+        {
+            case WindowID.Settings:
+                return FindObjectOfType<SettingsWindow>();
+            case WindowID.Creators:
+                return FindObjectOfType<CreatorsWindow>();
+            default:
+                return null;
+        }
+    }
 }
