04c5166b185043496723bf5848cc3b9f27fc521e
Nastia <adruts@mail.ru>
2024-01-26 23:31:04 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/BulletControllerEnemy.cs
+++ Assets/ScriptableObject/BulletControllerEnemy.cs
@@ -2,12 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletControllerEnemy : MonoBehaviour
+public class BulletControllerEnemy : MonoBehaviour, IPoolable
 {
     public float speed = 100; // Скорость пули
+    private BulletPool bulletPool;
+    private GameObject bullet;
+
+    void Start()
+    {
+        bulletPool = FindObjectOfType<BulletPool>();
+        bullet = bulletPool.GetWithTag("lazerGun1");
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.back * speed * Time.deltaTime);
+        bullet.transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+
+    public void OnRelease()
+    {
+        bulletPool.Release(bullet);
     }
 }
