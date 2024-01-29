27f25ea5ff1f033f70d91745bfecffdbf42bfec9
Nastia <adruts@mail.ru>
2024-01-29 17:29:01 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/BulletControllerEnemy.cs
+++ Assets/ScriptableObject/BulletControllerEnemy.cs
@@ -6,8 +6,24 @@ public class BulletControllerEnemy : MonoBehaviour
 {
     public float speed = 100; // Скорость пули
 
+    private ObjectPool objectPool;
+
+    private void Start()
+    {
+        objectPool = FindObjectOfType<ObjectPool>();
+    }
+
     void Update()
     {
         transform.Translate(Vector3.back * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Вызываем метод возвращения объекта в пул
+            objectPool.Release(gameObject);
+        }
     }
 }
