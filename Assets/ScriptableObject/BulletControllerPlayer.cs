4b86dbaa506f21c0e07394e5c3d2001eca685360
Nastia <adruts@mail.ru>
2024-01-30 15:09:17 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/ScriptableObject/BulletControllerPlayer.cs
+++ Assets/ScriptableObject/BulletControllerPlayer.cs
@@ -2,12 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletControllerPlayer : MonoBehaviour
+public class BulletControllerPlayer : MonoBehaviour, IPoolable
 {
     public float speed = 100; // Скорость пули
+    
+    private ObjectPool bulletPool;
+
+    private void Awake()
+    {
+        bulletPool = FindObjectOfType<ObjectPool>();
+    }
 
     void Start()
     {
         GetComponent<Rigidbody>().velocity = new Vector3(0, 0, speed);
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Enemy enemy = other.GetComponent<Enemy>();
+            enemy.health -= 5;
+
+            if (enemy.health <= 0)
+            {
+                enemy.OnRelease();
+                bulletPool.Release(this);
+            }
+        }
+    }
+
+    public void OnRelease()
+    {
+        gameObject.SetActive(false);
     }
 }
