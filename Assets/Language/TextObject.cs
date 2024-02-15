7f9796bce9b9c87d756b1f14064031bf3d01571d
Nastia <adruts@mail.ru>
2024-02-15 13:40:03 +0300

    https://github.com/anastyadru/GAMESpaceWorld.git

--- Assets/Language/TextObject.cs
+++ Assets/Language/TextObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "TextObject", menuName = "Localization/TextObject")]
+[CreateAssetMenu(fileName = "New Text Object", menuName = "Localization/TextObject")]
 public class TextObject : ScriptableObject
 {
     public Dictionary<string, string> texts = new Dictionary<string, string>();
